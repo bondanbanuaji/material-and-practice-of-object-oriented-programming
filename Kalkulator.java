748770ba5ef84901d34331518dd27094a0e5309e
Boba? <bondanbanuaji@gmail.com>
2025-02-20 17:23:17 +0700

    first commit

---
+++ Kalkulator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Kalkulator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        double angka1 = scanner.nextDouble();
+
+        System.out.println("Masukkan operator (+, -, *, /): ");
+        String operator = scanner.next();
+
+        System.out.print("Masukkan angka: ");
+        double angka2 = scanner.nextDouble();
+
+        double hasil;
+
+        switch (operator) {
+            case "+":
+                hasil = angka1 + angka2;
+                break;
+            case "-":
+                hasil = angka1 - angka2;
+                break;
+            case "*":
+                hasil = angka1 * angka2;
+                break;
+            case "/":
+                if (angka2 == 0) {
+                    System.out.println("Error: Pembagian dengan nol tidak diperbolehkan.");
+                    return;
+                }
+                hasil = angka1 / angka2;
+                break;
+            default:
+                System.out.println("Error: Operator salah");
+                return;
+        }
+
+        System.out.println("Hasilnya adalah : " + hasil);
+        scanner.close();
+    }
+}
