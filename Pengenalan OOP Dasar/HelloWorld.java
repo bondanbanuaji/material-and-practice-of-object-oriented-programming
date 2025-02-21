f5c3a81182010dfb1d88234516c3e5e82b00a963
Boba? <bondanbanuaji@gmail.com>
2025-02-21 15:02:32 +0700

    HELLO WORLD FIRST

---
+++ Pengenalan OOP Dasar/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println("Hello world");
+        System.out.println("Ini adalah pertama kalinya saya belajar pemrograman bahasa java");
+        System.out.println("Semoga saya diberikan kelancaran dalam belajar");
+        System.out.println("LET'S GO");
+        System.out.println("================================");
+    }
+    
+}
